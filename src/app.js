c7015dc572e8aac7a71fae0ce14fc6950c9569f2
paul <spammer4@planetsugar.co.uk>
2018-12-30 07:08:34 +0000

    Added transactions, profile and additional routes

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/", (req, res) => res.render("index", { title: "Index "}));
+// Read in user data 
+const accountData = fs.readFileSync(path.join(__dirname,"json/accounts.json"),"utf8");
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(path.join(__dirname,"json/users.json"),"utf8");
+const users = JSON.parse(userData);
+
+// routes
+app.get("/", (req, res) => res.render("index", { title: "Account Summary", accounts}));
+app.get("/savings", (req, res) => res.render("account", { account: accounts.savings }));
+app.get("/checking", (req, res) => res.render("account", { account: accounts.checking }));
+app.get("/credit", (req, res) => res.render("account", { account: accounts.credit }));
+app.get("/profile", (req,res) => res.render("profile",{ user: users[0] }));
+
 app.listen(3000, () => console.log("Server is running on port 3000"));
