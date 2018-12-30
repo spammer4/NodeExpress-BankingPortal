ed204992c07062bcf75f1c252d37d75687be8646
paul <spammer4@planetsugar.co.uk>
2018-12-30 09:16:41 +0000

    Added transfer and payment

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+// Setup
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true })); 
 
 // Read in user data 
 const accountData = fs.readFileSync(path.join(__dirname,"json/accounts.json"),"utf8");
@@ -19,5 +21,20 @@ app.get("/savings", (req, res) => res.render("account", { account: accounts.savi
 app.get("/checking", (req, res) => res.render("account", { account: accounts.checking }));
 app.get("/credit", (req, res) => res.render("account", { account: accounts.credit }));
 app.get("/profile", (req,res) => res.render("profile",{ user: users[0] }));
-
+app.get("/transfer", (req,res) => res.render("transfer"));
+app.post("/transfer", (req, res) => {
+    accounts[req.body.from].balance -= parseInt(req.body.amount);
+    accounts[req.body.to].balance += parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,"json/accounts.json"),accountsJSON,"utf8");
+    res.render("transfer",{ message: "Transfer Completed" });
+});
+app.get("/payment", (req,res) => res.render("payment", { account: accounts.credit }));
+app.post("/payment", (req, res) => {
+    accounts.credit.balance -= parseInt(req.body.amount);
+    accounts.credit.available += parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,"json/accounts.json"),accountsJSON,"utf8");
+    res.render("payment",{ message: "Payment Successful", account: accounts.credit });
+});
 app.listen(3000, () => console.log("Server is running on port 3000"));
