0c17db2936016be2d0bea91934d77693ad7664e1
paul <spammer4@planetsugar.co.uk>
2019-01-05 07:09:42 +0000

    Created data layer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+
+// Local libraries
+const data = require("./data");
+
+// Read the data back in and set up express
+const { writeJSON, accounts, users } = data;
 const app = express();
 
 // Setup
@@ -8,12 +14,6 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({ extended: true })); 
-
-// Read in user data 
-const accountData = fs.readFileSync(path.join(__dirname,"json/accounts.json"),"utf8");
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync(path.join(__dirname,"json/users.json"),"utf8");
-const users = JSON.parse(userData);
 
 // routes
 app.get("/", (req, res) => res.render("index", { title: "Account Summary", accounts}));
@@ -25,16 +25,14 @@ app.get("/transfer", (req,res) => res.render("transfer"));
 app.post("/transfer", (req, res) => {
     accounts[req.body.from].balance -= parseInt(req.body.amount);
     accounts[req.body.to].balance += parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,"json/accounts.json"),accountsJSON,"utf8");
+    writeJSON();
     res.render("transfer",{ message: "Transfer Completed" });
 });
 app.get("/payment", (req,res) => res.render("payment", { account: accounts.credit }));
 app.post("/payment", (req, res) => {
     accounts.credit.balance -= parseInt(req.body.amount);
     accounts.credit.available += parseInt(req.body.amount);
-    const accountsJSON = JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,"json/accounts.json"),accountsJSON,"utf8");
+    writeJSON();
     res.render("payment",{ message: "Payment Successful", account: accounts.credit });
 });
 app.listen(3000, () => console.log("Server is running on port 3000"));
